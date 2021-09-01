9c113491c8a3af3211fc7451e4e0a004251396fa
Gaurav Kumar <78679960+Gauravk9870@users.noreply.github.com>
2021-09-01 17:32:45 +0530

    Update Facebook Login.py

--- Facebook Login.py
+++ Facebook Login.py
@@ -1,5 +1,6 @@
 from tkinter import *
 
+#Creating a Window with help of Tk() function
 window = Tk()
 window.config(bg="#E8E8E8")
 width = window.winfo_screenwidth()
@@ -7,6 +8,7 @@ height = window.winfo_screenheight()
 
 window.geometry("%dx%d" % (width, height))
 
+#Facebook Label and Tagline
 facebook = Label(window, text="facebook",bg="#E8E8E8", font=("Klavika", 50, 'bold'), fg="#427bff")
 facebook.place(x=150, y=210)
 
@@ -16,9 +18,11 @@ tagline1.place(x=150, y=290)
 tagline2 = Label(window, text="with the people in your life.", bg="#E8E8E8" , font="Leelawadee 20 ", fg="black")
 tagline2.place(x=150, y=323)
 
+#Creating a Canvas for widgets
 canvas = Canvas(window, width=415, height=320, bg="#fffcfc")
 canvas.place(x=750, y=175)
 
+#Creating section for username and password
 username = Entry(window, width=28, font=("Verdana", 15), bg="#E8E8E8", fg="#999999")
 username.insert(10, 'Mobile number or email address')
 username.place(x=775, y=207, height=50)
@@ -27,22 +31,23 @@ password = Entry(window, width=28, font=("Verdana", 15),  bg="#E8E8E8", fg="#999
 password.insert(10, 'Password')
 password.place(x=775, y=267, height=50)
 
+#Creating Login Button
 login = Button(window, text="                  Login                ", bg="#008ad3", font=("Klavika", 20, 'bold'), fg="white", height=0, border=0)
 login.place(x=775, y=327, height=50)
 
 forget = Button(window, text="Forgotten Password?", bg="white", font=("Klavika", 10), fg="#008ad3", height=1, border=0)
 forget.place(x=900, y=377, height=20)
 
-forget = Button(window, text="Create New Account", bg="#93c572", font=("Klavika", 20, 'bold'), fg="white", height=1, border=0)
-forget.place(x=820, y=420, height=50)
+#Button for New Accoun
+newAcc = Button(window, text="Create New Account", bg="#93c572", font=("Klavika", 20, 'bold'), fg="white", height=1, border=0)
+newAcc.place(x=820, y=420, height=50)
 
-forget = Button(window, text="Create a Page", bg="#E8E8E8" ,font=("Klavika", 10, "bold"), fg="black", height=1, border=0)
-forget.place(x=810, y=500, height=20)
+#Button for Creating a Page
+page = Button(window, text="Create a Page", bg="#E8E8E8" ,font=("Klavika", 10, "bold"), fg="black", height=1, border=0)
+page.place(x=810, y=500, height=20)
 
 label = Label(window, text="for a celebrity, band or business", bg="#E8E8E8" ,font=("Klavika", 10), fg="black")
 label.place(x=910, y=500)
-
-
 
 
 mainloop()
